9eb6c9b57355b95009ba4175c63217912bb2ab26
gp <geoffrey.privard@gmail.com>
2020-12-16 14:28:09 +0100

    début tp7

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -21,6 +21,7 @@ class UserRepository extends ServiceEntityRepository implements PasswordUpgrader
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, User::class);
+        $this->blocked = false;
     }
 
     /**
